e813834c66beb22fad2302bdc17921a9ded3adcf
Richard Klafter <rpklafter@yahoo.com>
2014-08-30 22:53:26 -0700

    connect file example thing

--- src/wikiget.js
+++ src/wikiget.js
@@ -5,7 +5,7 @@ var ctrl = require('ctrl');
 var urlResolve = require('url').resolve;
 
 //Config Shits!
-request.defaults({'proxy':'http://tron.alloscomp.com:8090'});
+request = request.defaults({'proxy':'http://tron.alloscomp.com:8090'});
 
 
 function toArray(obj) {
